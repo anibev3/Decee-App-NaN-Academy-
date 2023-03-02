d3032695cd475aa644e2632e13832f52e8f89757
anibe201 <anibe201@gmail.com>
2023-03-02 20:38:14 +0000

    The last commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,28 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:jeu_de_dee_section7/screen/dice.screen.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+  return runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: const Text('Dicee'),
+          title: Text('Dicee'),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
       ),
-    );
-  }
+    ),
+  );
 }
--- lib/screen/dice.screen.dart
+++ lib/screen/dice.screen.dart
@@ -1,33 +1,47 @@
-import 'package:flutter/material.dart';
 import 'dart:math';
 
-// ignore: must_be_immutable
-class DicePage extends StatelessWidget {
-  const DicePage({super.key});
+import 'package:flutter/material.dart';
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    var leftDiceNumber = 4;
     return Center(
       child: Row(
-        children: [
+        children: <Widget>[
           Expanded(
             child: TextButton(
-                onPressed: () {
-                  SetState() {
-                    leftDiceNumber = Random().nextInt(6) + 1;
-                  }
-                },
-                child: Image.asset("assets/images/dice$leftDiceNumber.png")),
+              child: Image.asset(
+                'assets/images/dice$leftDiceNumber.png',
+              ),
+              onPressed: () {
+                changeDiceFace();
+              },
+            ),
           ),
           Expanded(
             child: TextButton(
-                onPressed: () {
-                  SetState() {
-                    leftDiceNumber = Random().nextInt(6) + 1;
-                  }
-                },
-                child: Image.asset("assets/images/dice1.png")),
+              child: Image.asset(
+                'assets/images/dice$rightDiceNumber.png',
+              ),
+              onPressed: () {
+                changeDiceFace();
+              },
+            ),
           ),
         ],
       ),
